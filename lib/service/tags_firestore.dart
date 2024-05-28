801a79f773cdc7c3a65699261aa9d357cd265b75
fihrizilhamr <116176265+fihrizilhamr@users.noreply.github.com>
2024-05-28 16:22:23 +0700

    tags_firestore

---
+++ lib/service/tags_firestore.dart
@@ -0,0 +1,87 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'lib\service\notes_firestore.dart';
+
+class FirestoreService {
+  final CollectionReference notes = FirebaseFirestore.instance.collection('notes');
+  final CollectionReference tags = FirebaseFirestore.instance.collection('tags');
+
+  Future<void> addNoteWithTags(String note, List<String> tagsList) async {
+    DocumentReference noteRef = await notes.add({
+      'note': note,
+      'timestamp': Timestamp.now(),
+    });
+    for (String tag in tagsList) {
+      await tags.add({
+        'noteId': noteRef.id,
+        'tag': tag,
+      });
+    }
+  }
+
+  Future<void> addTagsToNote(String docID, List<String> tagsList) async {
+    for (String tag in tagsList) {
+      await tags.add({
+        'noteId': docID,
+        'tag': tag,
+      });
+    }
+  }
+
+  Future <void> updateNoteWithTags(String docID, String newNote, List<String> newTagsList) async {
+    await notes.doc(docID).update(
+        {
+          'note': newNote,
+          'timestamp': Timestamp.now(),
+        }
+    );
+    QuerySnapshot oldTags = await tags.where('noteId', isEqualTo: docID).get();
+    for (QueryDocumentSnapshot doc in oldTags.docs) {
+      await doc.reference.delete();
+    }
+
+    for (String tag in newTagsList) {
+      await tags.add({
+        'noteId': docID,
+        'tag': tag,
+      });
+    }
+  }
+
+    Future <void> updateTagsToNote(String docID, List<String> newTagsList) async {
+    QuerySnapshot oldTags = await tags.where('noteId', isEqualTo: docID).get();
+    for (QueryDocumentSnapshot doc in oldTags.docs) {
+      await doc.reference.delete();
+    }
+    
+    for (String tag in newTagsList) {
+      await tags.add({
+        'noteId': docID,
+        'tag': tag,
+      });
+    }
+  }
+
+  Future<void> deleteNoteWithTags(String docID) async {
+    await notes.doc(docID).delete();
+
+    QuerySnapshot noteTags = await tags.where('noteId', isEqualTo: docID).get();
+    for (QueryDocumentSnapshot doc in noteTags.docs) {
+      await doc.reference.delete();
+    }
+  }
+
+  Future<void> deleteTagsFromNote(String docID, List<String> newTagsList) async {
+    QuerySnapshot oldTagsSnapshot = await tags.where('noteId', isEqualTo: docID).get();
+    for (var doc in oldTagsSnapshot.docs) {
+      String oldTag = doc['tag'];
+      if (newTagsList.contains(oldTag)) {
+        await doc.reference.delete();
+      }
+    }
+  }
+
+  Future<List<String>> getTagsForNoteId(String noteId) async {
+    QuerySnapshot tagSnapshot = await tags.where('noteId', isEqualTo: noteId).get();
+    return tagSnapshot.docs.map((doc) => doc['tag'] as String).toList();
+  }
+}
